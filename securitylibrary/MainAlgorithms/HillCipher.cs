7f0528b8dd92a0c5ec8f4763e945da93aa2821c4
karensamuel <113302396+karensamuel@users.noreply.github.com>
2025-03-04 20:37:52 +0200

    Update HillCipher.cs
    decryption and encryption are done

--- securitylibrary/MainAlgorithms/HillCipher.cs
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -13,19 +13,145 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(List<int> plainText, List<int> cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+           // List<int> key =new List<int>();
+            List<int> inverseplain = new List<int> {
+
+             plainText[0], plainText[1], plainText[2], plainText[3]
+            
+            };
+            List<int> cipher = new List<int> {
+
+             cipherText[0], cipherText[1], cipherText[2], cipherText[3]
+
+            };
+            inverseplain = inverse2(inverseplain);
+           // int sum = 0;
+            List<int> key = new List<int>
+            {
+                 (cipher[0] * inverseplain[0] + cipher[1] * inverseplain[2]) % 26,
+                 (cipher[0] * inverseplain[1] + cipher[1] * inverseplain[3]) % 26,
+                 (cipher[2] * inverseplain[0] + cipher[3] * inverseplain[2]) % 26,
+                 (cipher[2] * inverseplain[1] + cipher[3] * inverseplain[3]) % 26
+            };
+
+            if (key.Count != 4)
+            {
+                throw new InvalidAnlysisException();
+            }
+            return key;
         }
 
 
         public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int size = key.Count();
+            int result = (int)Math.Sqrt(size);
+            List<int> DeCypheredText = new List<int>();
+            List<int> keyinverse;
+            if (key.Count == 4) {
+                keyinverse = inverse2(key);
+            }
+            else
+            {
+               keyinverse = inverse3(key);
+            }
+            
+            if (result * result != size) throw new Exception("wrong key size");
+           
+            return Encrypt(cipherText,keyinverse);
         }
+        public List<int> inverse2(List<int> key)
+        {
+            int deter = (key[0] * key[3] - key[1] * key[2]) % 26;
+            if (deter < 0) deter += 26;
+            int inver = -1;
 
+            for (int x = 1; x < 26; x++)
+            {
+                if ((deter * x) % 26 == 1)
+                {
+                    inver = x;
+                    break;
+                }
+            }
+            if (inver == -1) throw new InvalidAnlysisException();
+            List<int> keyinverse = new List<int>
+            {
+               (key[3] * inver) % 26,
+        (-key[1] * inver + 26) % 26,
+        (-key[2] * inver + 26) % 26,
+        (key[0] * inver) % 26
+             };
+            return keyinverse;
+        }
+        public List<int> inverse3(List<int> Text)
+        {
+            List<int> inverse = new List<int>();
+            int size = Text.Count();
+            size = (int)Math.Sqrt((double)size);
+           
+            int det = (Text[0] * (Text[4] * Text[8] - Text[5] * Text[7]))
+                - (Text[1] * (Text[3] * Text[8] - Text[5] * Text[6]))
+                +(Text[2] * (Text[3] * Text[7] - Text[4] * Text[6]));
+            
+            det = ((det % 26) + 26) % 26;
+            int inver = -1;
+
+            for (int x = 1; x < 26; x++)
+            {
+                if ((det * x) % 26 == 1)
+                {
+                    inver = x;
+                    break;
+                }
+            }
+            if (inver == -1) throw new InvalidAnlysisException();
+            List<int> adj = new List<int>
+    {
+        ((Text[4] * Text[8] - Text[5] * Text[7]) % 26 + 26) % 26,
+        ((-1 * (Text[1] * Text[8] - Text[2] * Text[7])) % 26 + 26) % 26,
+        ((Text[1] * Text[5] - Text[2] * Text[4]) % 26 + 26) % 26,
+        ((-1 * (Text[3] * Text[8] - Text[5] * Text[6])) % 26 + 26) % 26,
+        ((Text[0] * Text[8] - Text[2] * Text[6]) % 26 + 26) % 26,
+        ((-1 * (Text[0] * Text[5] - Text[2] * Text[3])) % 26 + 26) % 26,
+        ((Text[3] * Text[7] - Text[4] * Text[6]) % 26 + 26) % 26,
+        ((-1 * (Text[0] * Text[7] - Text[1] * Text[6])) % 26 + 26) % 26,
+        ((Text[0] * Text[4] - Text[1] * Text[3]) % 26 + 26) % 26
+
+    };
+            for (int i = 0; i < 9; i++)
+            {
+                int x = (adj[i]*inver) % 26;
+                if (x < 0) x += 26;
+                inverse.Add( x);
+            }
+         
+            return inverse;
+        }
 
         public List<int> Encrypt(List<int> plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+           int size= key.Count();
+            int result = (int)Math.Sqrt((double)size);
+            List<int> CypheredText= new List<int>();
+            int sum = 0;
+            for(int i=0; i < plainText.Count(); i+=result)
+            {
+                for (int r = 0; r < result; r++)
+                {
+                    sum = 0;
+                    for(int c = 0;c<result; c++)
+                    {
+                        sum += key[r*result+c]*plainText[i+c];
+                    }
+
+                    CypheredText.Add((sum % 26 + 26) % 26);
+                }
+            }
+            return CypheredText;
         }
 
 
