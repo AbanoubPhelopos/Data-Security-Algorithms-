1658f62f8bcd33f79a8e9b08a0ae1190287e64be
karensamuel <113302396+karensamuel@users.noreply.github.com>
2025-04-24 00:10:43 +0200

    finish m3
    extended eculidean

--- securitylibrary/AES/ExtendedEuclid.cs
+++ securitylibrary/AES/ExtendedEuclid.cs
@@ -16,7 +16,37 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int a1 = 1, a2 = 0, a3 = baseN;
+            int b1 = 0, b2 = 1, b3 = number;
+            int q, t1, t2, t3;
+            while (true)
+            {
+                if (b3 == 0) return -1;
+                else if (b3 == 1)
+                {
+                    //3shan at2kd en al natiga  postive
+                    int result = b2 % baseN;
+                    if (result < 0)
+                    {
+                        result += baseN;
+                    }
+
+                    Console.WriteLine("acutal result kk : " + result);
+                    return result;
+                }
+                q = a3 / b3;
+                t1 = a1 - q * b1;
+                t2 = a2 - q * b2;
+                t3 = a3 - q * b3;
+                a1 = b1; a2 = b2; a3 = b3;
+                b1 = t1; b2 = t2; b3 = t3;
+            }
+
+
         }
     }
+
+
 }
+    
